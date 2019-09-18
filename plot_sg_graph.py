21c640182ee12047e3b0d257ee7e43eba29ab982
Andrew Wei <andrew.wei@affirm.com>
2019-09-18 14:39:37 -0700

    add arg parsing

--- plot_sg_graph.py
+++ plot_sg_graph.py
@@ -1,78 +1,90 @@
 import boto3
 import pydot
+import argparse
 
 from sg_graph import security_group_graph
 
-# TODO: Implement argument parsing to pass in AWS profiles + output naming
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Script for printing AWS security group rules to csv')
+    parser.add_argument('-p', '--profile_name', type=str, help='AWS profile to use.')
+    parser.add_argument('-o', '--output_file', type=str,
+                        help='name for output file.', default= 'sg_inbound_rules.png')
+    return parser.parse_args()
 
 
-session = boto3.session.Session()
-ec2_client = session.client('ec2')
+def main():
+    args = parse_args()
+    PROFILE_NAME = args.profile_name
+    OUTPUT_FILE = args.output_file
 
-#### get all security group JSON data
-security_groups_json = ec2_client.describe_security_groups()
-security_groups_json = security_groups_json['SecurityGroups']
+    session = boto3.session.Session(profile_name=PROFILE_NAME)
+    ec2_client = session.client('ec2')
 
-security_groups = {x['GroupId']: x['GroupName'] for x in security_groups_json}
+    #### get all security group JSON data
+    security_groups_json = ec2_client.describe_security_groups()
+    security_groups_json = security_groups_json['SecurityGroups']
 
-
-#### get list of active security groups
-ni_json = ec2_client.describe_network_interfaces()
-ni_security_groups = a = [[x['GroupId'] for x in y['Groups']] for y in ni_json['NetworkInterfaces']]
-ni_security_groups = [x for sublist in ni_security_groups for x in sublist]
-ni_security_groups = set(ni_security_groups)
-
-#### filter security_groups_json to only include active groups
-active_groups_json = [x for x in security_groups_json if x['GroupId'] in ni_security_groups]
+    security_groups = {x['GroupId']: x['GroupName'] for x in security_groups_json}
 
 
-graph = security_group_graph(active_groups_json)
+    #### get list of active security groups
+    ni_json = ec2_client.describe_network_interfaces()
+    ni_security_groups = a = [[x['GroupId'] for x in y['Groups']] for y in ni_json['NetworkInterfaces']]
+    ni_security_groups = [x for sublist in ni_security_groups for x in sublist]
+    ni_security_groups = set(ni_security_groups)
+
+    #### filter security_groups_json to only include active groups
+    active_groups_json = [x for x in security_groups_json if x['GroupId'] in ni_security_groups]
 
 
-def convert_to_printable(graph):
-    printable_graph = {}
-    for group_id, rules in graph.items():
-        printable_graph[f'{group_id}\n{security_groups[group_id]}'] = rules
-
-    for node, links in printable_graph.items():
-        new_links = []
-        for link in links:
-            if link.source[0] == 's':   # security group
-                new_link = f'{link.source}\n{security_groups[link.source]}'
-            else:                       # ip
-                new_link = f'{link.source}\n{link.description if link.description else ""}'
-            new_links.append(new_link)
-            printable_graph[node] = new_links
-
-    return printable_graph
-
-printable_graph = convert_to_printable(graph)
+    graph = security_group_graph(active_groups_json)
 
 
-#### Create pydot graph
-nodes = []
-for group in printable_graph.keys():
-    nodes.append(group)
-print(f'{len(nodes)} active security groups found.')
+    def convert_to_printable(graph):
+        printable_graph = {}
+        for group_id, rules in graph.items():
+            printable_graph[f'{group_id}\n{security_groups[group_id]}'] = rules
 
-edges = []
-for node , links in printable_graph.items():
-    node_edges = [(node, link) for link in links]
-    edges += node_edges
-edges = set(edges)
-print(f'{len(edges)} edges found.')
+        for node, links in printable_graph.items():
+            new_links = []
+            for link in links:
+                if link.source[0] == 's':   # security group
+                    new_link = f'{link.source}\n{security_groups[link.source]}'
+                else:                       # ip
+                    new_link = f'{link.source}\n{link.description if link.description else ""}'
+                new_links.append(new_link)
+                printable_graph[node] = new_links
 
-pydot_nodes = [pydot.Node(x) for x in nodes]
-pydot_edges = [pydot.Edge(x[0],x[1], dir='back') for x in edges]
+        return printable_graph
 
-G = pydot.Dot(graph_type='digraph', rankdir='LR')
+    printable_graph = convert_to_printable(graph)
 
-for n in pydot_nodes:
-    G.add_node(n)
-for e in pydot_edges:
-    G.add_edge(e)
 
-G.write_png('graph.png')
-print('graph saved to graph.png')
+    #### Create pydot graph
+    nodes = []
+    for group in printable_graph.keys():
+        nodes.append(group)
+    print(f'{len(nodes)} active security groups found.')
+
+    edges = []
+    for node , links in printable_graph.items():
+        node_edges = [(node, link) for link in links]
+        edges += node_edges
+    edges = set(edges)
+    print(f'{len(edges)} edges found.')
+
+    pydot_nodes = [pydot.Node(x) for x in nodes]
+    pydot_edges = [pydot.Edge(x[0],x[1], dir='back') for x in edges]
+
+    G = pydot.Dot(graph_type='digraph', rankdir='LR')
+
+    for n in pydot_nodes:
+        G.add_node(n)
+    for e in pydot_edges:
+        G.add_edge(e)
+
+    G.write_png(OUTPUT_FILE)
+    print(f'graph saved to {OUTPUT_FILE}')
 
 
--- rules_to_csv.py
+++ rules_to_csv.py
@@ -1,10 +1,15 @@
 import boto3
 import pandas as pd
+import argparse
 
 from sg_graph import security_group_graph
 
-# TODO: Implement argument parsing to pass in AWS profiles + output naming
 
+def parse_args():
+    # TODO: Implement argument parsing for output naming
+    parser = argparse.ArgumentParser(description='Script for printing AWS security group rules to csv')
+    parser.add_argument('-p', '--profile_name', type=str, help='AWS profile to use.')
+    return parser.parse_args()
 
 def get_tag_value(resource_json, key):
     if not resource_json.get('Tags'):
@@ -17,69 +22,75 @@ def get_tag_value(resource_json, key):
     else:
         return value[0]
 
+def main():
+    args = parse_args()
+    PROFILE_NAME = args.profile_name
 
-session = boto3.session.Session()
-ec2_client = session.client('ec2')
-#ec2 = session.resource('ec2')
+    # set up AWS session + EC2 client
+    session = boto3.session.Session(profile_name=PROFILE_NAME)
+    ec2_client = session.client('ec2')
+    # ec2 = session.resource('ec2')
 
-security_groups_json = ec2_client.describe_security_groups()
-security_groups_json = security_groups_json['SecurityGroups']
+    # create security group graph
+    security_groups_json = ec2_client.describe_security_groups()
+    security_groups_json = security_groups_json['SecurityGroups']
+    security_groups = {x['GroupId']: {'GroupName': x['GroupName'],
+                                      'Description': x['Description'],
+                                      'Name': get_tag_value(x, 'Name'),
+                                      'owner': get_tag_value(x, 'owner'),
+                                      'team': get_tag_value(x, 'team'),
+                                      'role': get_tag_value(x, 'role'),
+                                      }
+                       for x in security_groups_json}
+    graph = security_group_graph(security_groups_json)
+
+    #### Create table of security group rules
+    rule_records = []
+    for sg, rules in graph.items():
+        for rule in rules:
+            rule_records.append([sg, security_groups[sg], rule.source, rule.description, rule.port, rule.protocol])
+    records_dict = dict(zip(range(len(rule_records)), rule_records))
+
+    df = pd.DataFrame.from_dict(records_dict, orient='index',
+                                columns=['GroupId', 'GroupName', 'Source', 'Description', 'Port', 'Protocol'])
+    print(f'{len(df)} security group rules found')
+    print('saving rules to inbound_rules.csv')
+    df.to_csv('inbound_rules.csv')
 
 
-security_groups = {x['GroupId']: {'GroupName': x['GroupName'],
-                                  'Description': x['Description'],
-                                  'Name': get_tag_value(x, 'Name'),
-                                  'owner': get_tag_value(x, 'owner'),
-                                  'team': get_tag_value(x, 'team'),
-                                  'role': get_tag_value(x, 'role'),
-                                  }
-                            for x in security_groups_json}
+    #### Create table of security groups + # of interfaces attached to each
+    ni_json = ec2_client.describe_network_interfaces()
+    # active_security_groups = [[x['GroupId'] for x in y['Groups']] for y in ni_json['NetworkInterfaces']]
+    # active_security_groups = [x for sublist in active_security_groups for x in sublist]
+    # active_security_groups = set(active_security_groups)
 
-graph = security_group_graph(security_groups_json)
+    sg_records = []
+    for group_id, group_data in security_groups.items():
+        n_attached = len([x for x in ni_json['NetworkInterfaces'] if group_id in [y['GroupId'] for y in x['Groups']]])
+        sg_records.append([group_id,
+                           group_data['GroupName'],
+                           group_data['Description'],
+                           n_attached,
+                           group_data['Name'],
+                           group_data['owner'],
+                           group_data['team'],
+                           group_data['role']
+                           ])
+
+    records_dict = dict(zip(range(len(sg_records)), sg_records))
+
+    df = pd.DataFrame.from_dict(records_dict, orient='index', columns=['GroupId',
+                                                                       'GroupName',
+                                                                       'Description',
+                                                                       'Interfaces_attached',
+                                                                       'Name',
+                                                                       'owner',
+                                                                       'team',
+                                                                       'role'])
+    print(f'{len(df)} security groups found')
+    print('saving security group data to security_groups.csv')
+    df.to_csv('security_groups.csv')
 
 
-#### Create table of security group rules
-rule_records = []
-for sg, rules in graph.items():
-    for rule in rules:
-        rule_records.append([sg, security_groups[sg], rule.source, rule.description, rule.port, rule.protocol])
-records_dict = dict(zip(range(len(rule_records)), rule_records))
-
-df = pd.DataFrame.from_dict(records_dict, orient='index', columns=['GroupId','GroupName','Source','Description','Port','Protocol'])
-print(f'{len(df)} security group rules found')
-df.to_csv('inbound_rules.csv')
-print('saving rules to inbound_rules.csv')
-
-
-#### Create table of security groups + # of interfaces attached to each
-ni_json = ec2_client.describe_network_interfaces()
-active_security_groups = a = [[x['GroupId'] for x in y['Groups']] for y in ni_json['NetworkInterfaces']]
-active_security_groups = [x for sublist in active_security_groups for x in sublist]
-active_security_groups = set(active_security_groups)
-
-sg_records = []
-for group_id, group_data in security_groups.items():
-    n_attached = len([x for x in ni_json['NetworkInterfaces'] if group_id in [y['GroupId'] for y in x['Groups']]])
-    sg_records.append([group_id,
-                    group_data['GroupName'],
-                    group_data['Description'],
-                    n_attached,
-                    group_data['Name'],
-                    group_data['owner'],
-                    group_data['team'],
-                    group_data['role']
-                   ])
-
-records_dict = dict(zip(range(len(sg_records)), sg_records))
-
-df = pd.DataFrame.from_dict(records_dict, orient='index', columns=['GroupId',
-                                                                   'GroupName',
-                                                                   'Description',
-                                                                   'Interfaces_attached',
-                                                                   'Name',
-                                                                   'owner',
-                                                                   'team',
-                                                                   'role'])
-print(f'{len(df)} security groups found')
-df.to_csv('security_groups.csv')
-print('saving security group data to security_groups.csv')
+if __name__ == "__main__":
+    main()
